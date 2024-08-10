0dc8ace64350616a4c36c966cb339e4ccba79741
MuriloCB <murilo.esker@gmail.com>
2024-08-09 23:28:25 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::copy;
+use tempfile::Builder;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("HTTP request error: {0}")]
+    HttpRequest(#[from] reqwest::Error),
+}
+
+type Result<T> = std::result::Result<T, AppError>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.bin");
+
+        println!("file to download: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("will be located under: '{:?}'", fname);
+        File::create(fname)?
+    };
+
+    let content = response.bytes().await?;
+    copy(&mut content.as_ref(), &mut dest)?;
+    Ok(())
+}
